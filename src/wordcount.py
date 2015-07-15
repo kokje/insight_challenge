22f6d92a90eafeb3f970942b8d4bc9cfd78c8a5f
Kokje, Yashashree <Shree_Kokje@intuit.com>
2015-07-15 01:22:47 -0700

    Use property file in run script

--- src/wordcount.py
+++ src/wordcount.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import ConfigParser
 
 def setupAndTeardown():
 	try:
@@ -43,14 +44,24 @@ if __name__=='__main__':
 	if len(sys.argv) != 4:
 		print "Insufficient input parameters"
 	inputfile = sys.argv[1]
+
+	# Get config parameters from property file
+	configfile = sys.argv[3]
+	config = ConfigParser.RawConfigParser()
+	config.read(configfile)
+	numprocs = config.getint('Processes','numprocs')
+	shards = []
+	numshards = config.getint('Shards','numshards')
+	for i in range(0,numshards):
+		nameshard = 'shard'+str(i)
+		shards.append(config.get('Shards', nameshard))
+
 	# Use size of the input file and the number of processes to determine positions to read from in the file
 	size = os.path.getsize(inputfile)
-	numprocs = 3
 	seeklength = size/numprocs
 
 	#Shards are currently hardcoded
 	#TODO: Incorporate dynamic shards which are setup using configuration script
-	shards = ["characters","numbers","a_f","k_r","s_z"]
 	assert setupAndTeardown() == 0
 	assert countOccurences(inputfile,numprocs,seeklength) == 0
 	assert sortIntermediates(shards) == 0
